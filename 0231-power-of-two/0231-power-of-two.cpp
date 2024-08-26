d707fd3d9731ca39079cbcc65636d15c64e83c44
Silky Modi <99467488+silky-modi22@users.noreply.github.com>
2024-08-27 01:29:44 +0530

    Time: 0 ms (100%), Space: 7.5 MB (56.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) { 
-  for(int i=0;i<=30;i++){ 
-      int ans= pow(2,i); 
-      if(ans == n){ 
-          return true;
-      } 
-  } 
-  return false;       
+    // A number is a power of two if:
+    // 1. It is greater than 0.
+    // 2. It has exactly one bit set in its binary representation.
+    // This can be checked by the condition n > 0 && (n & (n - 1)) == 0.
+    return (n > 0) && (n & (n - 1)) == 0;     
     }
 };
